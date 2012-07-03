3bc23a2be044530ca353b9e74a1417675e1d2302
jchen <jchen@851f6010-ba38-4094-9a75-7393bf5d8ab7>
2012-07-03 15:58:24 +0000

    biogeme cmake

--- src/cmake/Modules/FindBiogeme.cmake
+++ src/cmake/Modules/FindBiogeme.cmake
@@ -4,48 +4,68 @@
 #   BIOGEME_INCLUDE_DIRECTORIES to directories required for using libpq
 #   BIOGEME_LIBRARIES           to libpq and any dependent libraries
 # If BIOGEME_REQUIRED is defined, then a fatal error message will be generated if libpq is not found
-if ( NOT BIOGEME_INCLUDE_DIRECTORIES OR NOT BIOGEME_LIBRARIES OR NOT BIOGEME_FOUND )
 
-  if ( $ENV{BIOGEME_DIR} )
-    file( TO_CMAKE_PATH "$ENV{BIOGEME_DIR}" _BIOGEME_DIR )
-  endif ( $ENV{BIOGEME_DIR} )
+#  if ( $ENV{BIOGEME_DIR} )
+#    file( TO_CMAKE_PATH "$ENV{BIOGEME_DIR}" _BIOGEME_DIR )
+#  endif ( $ENV{BIOGEME_DIR} )
+  SET(BIOGEME_POSSIBLE_ROOT_DIRS
+    ${BIOGEME_ROOT_DIR}
+    $ENV{BIOGEME_ROOT_DIR}
+    ${BIOGEME_DIR}
+    ${BIOGEME_HOME}
+    "/home/jchen/biogeme-2.0/libraries/parameters"
+    "/home/jchen/biogeme-2.0/libraries/utils"
+    "/home/jchen/biogeme-2.0/libraries/utils"
+    "/usr/local/lib/biogeme/"
+    "/home/jchen/biogeme-2.0/libraries/parameters"
+    "/home/jchen/biogeme-2.0"
+    "/data/jqdu/libs/lib/biogeme"
+    "/data/jqdu/libs/include/biogeme/"
+    "/data/jqdu/libs"
+    )
 
-  find_library( BIOGEME_LIBRARIES
-    NAMES BIOGEME libBIOGEME
+  find_library( LIB_UTILS
+    NAMES NAMES utils libutils patUtils libutils.la utils.la
     PATHS
-      ${_BIOGEME_DIR}/lib
-      ${CMAKE_INSTALL_PREFIX}/lib
-      /usr/local/lib
-      /usr/lib
-      ${_BIOGEME_DIR}
-      /users/jchen/libs/lib
-      ${CMAKE_INSTALL_PREFIX}/bin
-    NO_DEFAULT_PATH
+      ${BIOGEME_POSSIBLE_ROOT_DIRS}
   )
 
-  find_path( BIOGEME_INCLUDE_DIRECTORIES
-    NAMES shapefil.h
-    PATHS
-      ${_BIOGEME_DIR}
-      /users/jchen/libs/include
-      ${_BIOGEME_DIR}/include
-      ${CMAKE_INSTALL_PREFIX}/include
-      /usr/local/include
-      /usr/include
-    NO_DEFAULT_PATH
-  )
+    find_library(LIB_PARAMETERS
+                    NAMES param libparameters patParams parameters libparameters.la  parameters.la
+                    PATHS
+                          ${BIOGEME_POSSIBLE_ROOT_DIRS})
 
-  if ( NOT BIOGEME_INCLUDE_DIRECTORIES OR NOT BIOGEME_LIBRARIES ) 
+  find_path(BIOGEME_INCLUDE_DIRECTORIES
+    NAMES config.h
+    PATHS ${BIOGEME_POSSIBLE_ROOT_DIRS}
+    PATH_SUFFIXES include
+    DOC "biogeme header include dir"
+   )
+
+  find_path(BIOGEME_INCLUDE_UTILS_DIRECTORIES
+    NAMES patType.h patDisplay.h patString.h
+    PATHS ${BIOGEME_POSSIBLE_ROOT_DIRS}
+    PATH_SUFFIXES include
+    DOC "biogeme header include dir"
+   )
+
+  if ( NOT BIOGEME_INCLUDE_DIRECTORIES OR NOT LIB_UTILS OR NOT LIB_PARAMETERS )
     if ( BIOGEME_REQUIRED )
-      message( FATAL_ERROR "libshap is required. Set BIOGEME_DIR" )
+      message( FATAL_ERROR "biogeme is required. Set BIOGEME_DIR" )
     endif ( BIOGEME_REQUIRED )
-  else ( NOT BIOGEME_INCLUDE_DIRECTORIES OR NOT BIOGEME_LIBRARIES ) 
+  else ( NOT BIOGEME_INCLUDE_DIRECTORIES OR NOT LIB_UTILS  OR NOT LIB_PARAMETERS )
     set( BIOGEME_FOUND 1 )
-    message("libBIOGEME found")
+    message("biogeme found")
     mark_as_advanced( BIOGEME_FOUND )
-  endif ( NOT BIOGEME_INCLUDE_DIRECTORIES OR NOT BIOGEME_LIBRARIES )
+  endif (  NOT BIOGEME_INCLUDE_DIRECTORIES OR NOT LIB_UTILS  OR NOT LIB_PARAMETERS )
 
-endif ( NOT BIOGEME_INCLUDE_DIRECTORIES OR NOT BIOGEME_LIBRARIES OR NOT BIOGEME_FOUND )
+if (BIOGEME_FOUND)
+    message(${BIOGEME_INCLUDE_DIRECTORIES})
+    message(${BIOGEME_INCLUDE_UTILS_DIRECTORIES})
+    include_directories(${BIOGEME_INCLUDE_DIRECTORIES})
+    include_directories(${BIOGEME_INCLUDE_UTILS_DIRECTORIES})
 
+endif (BIOGEME_FOUND)
 mark_as_advanced( FORCE BIOGEME_INCLUDE_DIRECTORIES )
-mark_as_advanced( FORCE BIOGEME_LIBRARIES )
+mark_as_advanced( FORCE LIB_PARAMETERS )
+mark_as_advanced( FORCE LIB_UTILS )
